38cf992032629a878bb1236ad0967802b7d884fd
James <jjcastle@utexas.edu>
2022-05-24 20:42:39 -0500

    added definitions for getters and setters for age, first name, last name, and standard. Also began building to_string method to print all Student class attributes per spec.

--- main.cpp
+++ main.cpp
@@ -7,18 +7,42 @@ class Student {
         int age = 0;
         string firstName = "";
         string lastName = "";
-        string standard = "";
+        int standard = 0;
 
     public:
         //Getter and setter for age
-        void setAge (int inputAge) {
+        void set_age (int inputAge) {
             age = inputAge;
         }
+        int get_age () {
+            return age;
+        }
         //Getter and setter for first name
-
+        void set_first_name (string inputFName){
+            firstName = inputFName;
+        };
+        string get_first_name () {
+            return firstName;
+        }
         //Getter and setter for last name
-
+        void set_last_name (string inputLName) {
+            lastName = inputLName;
+        }
+        string get_last_name () {
+            return lastName;
+        }
+        //Getter and setter for standard
+        void set_standard (int inputStandard) {
+            standard = inputStandard;
+        }
+        int get_standard () {
+            return standard;
+        }
         //create to_string to print age,first name, last name, and standard
+        string to_string () {
+            string finalString = "";
+            return finalString;
+        }
 };
 
 int main() {
